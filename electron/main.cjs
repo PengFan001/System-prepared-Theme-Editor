bd09a45dfc0ea9ec9201e18442b9178a409ee011
PengFan001 <PengFan001@users.noreply.github.com>
2026-09-12 23:40:01 +0800

    fix: 补回丢失的 4 个 IPC handler（assets:import / list:scan / list:bind / list:coverage）
    冒烟日志发现打开项目时 list:scan 报 No handler registered，导致清单面板不可用

--- electron/main.cjs
+++ electron/main.cjs
@@ -126,6 +126,11 @@ ipcMain.handle('list:load', (_e, file) => loadList(file));
 ipcMain.handle('list:init', (_e, dirs, project) => generateList(dirs, project));
 ipcMain.handle('list:check', (_e, file) => checkList(loadList(file)));
 ipcMain.handle('assets:match', (_e, listFile, assetDir) => matchAssets(loadList(listFile), assetDir));
+ipcMain.handle('assets:import', (_e, themeDir, matched, opts) => importAssets(themeDir, matched, opts));
+ipcMain.handle('list:scan', (_e, themeDir) => scanLists(themeDir));
+ipcMain.handle('list:bind', (_e, themeDir, listFile) => bindList(themeDir, listFile));
+ipcMain.handle('list:coverage', (_e, themeDir, listFile, profile) =>
+  coverage(themeDir, loadList(listFile), profile));
 
 app.whenReady().then(() => {
   buildMenu();
